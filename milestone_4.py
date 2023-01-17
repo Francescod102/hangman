296ebc3b294ab0d4b4f3e0efd117f35d2e83a710
francescod102 <Francescodesimone910@gmail.com>
2023-01-17 21:21:45 +0000

    last review

--- milestone_4.py
+++ milestone_4.py
@@ -13,28 +13,27 @@ class Hangman():
 
 
     def check_guess(self, guess): #class methods
-        self.guess = guess
-        self.guess.lower()
-        print(self.word)
-        if self.guess in self.word:
-            print(f'Yes! {self.guess} is in the word, guess another.')
+    
+        if self.guess.lower() in self.word:
+            print(f'Good guess! {guess} is in the word.')
+        
             for i, character in enumerate(self.word):
-                if character == self.guess:
-                    self.word_guessed[i] = self.guess
+                if character == guess:
+                    self.word_guessed[i] = guess
             self.num_letters -= 1
-            print(self.word_guessed)
 
         else:
             self.num_lives -= 1
             print(f"sorry, {self.guess} is not in the world.")
             print(f"You have {self.num_lives} lives left.") 
         
-        self.list_of_guess.append(self.guess)
+        self.list_of_guess.append(guess)
+        print(self.word_guessed)
     
     def ask_for_input(self):
         while True:
-            self.guess = input('Please guess a letter. ')
-            if not self.guess.isalpha() and len(self.guess) != 1:
+            guess = input('Please guess a letter. ')
+            if not guess.isalpha() and len(guess) != 1:
                  print('Invalid letter. Please, enter a single alphabetical character.')
 
             elif self.guess in self.list_of_guess:
@@ -42,4 +41,5 @@ class Hangman():
 
             else:
                  self.check_guess(self.guess)
+                 break
                  
