19e589a8e9778c2f17fd5acc17ed9e060e2657c1
francescod102 <Francescodesimone910@gmail.com>
2023-01-28 15:52:08 +0000

    last

--- milestone_4.py
+++ milestone_4.py
@@ -9,7 +9,7 @@ class Hangman():
         self.word = random.choice(self.word_list)
         self.word_guessed = ["_"]* len(self.word)
         self.num_letters  = len(set(self.word))
-        self.list_of_guess= []  
+        self.list_of_guesses = []  
 
 
     def check_guess(self, guess): #class methods
