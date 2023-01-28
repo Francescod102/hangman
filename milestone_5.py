52eea7d2d995ff41f75928702ed429c21f6d4eb0
francescod102 <Francescodesimone910@gmail.com>
2023-01-28 16:19:54 +0000

    all code

--- milestone_5.py
+++ milestone_5.py
@@ -1,4 +1,49 @@
-from milestone_4 import Hangman
+import random
+
+
+class Hangman():
+
+    def __init__(self, word_list, num_lives=5):   #constructor
+        self.word_list = word_list
+        self.num_lives = num_lives
+        self.word = random.choice(self.word_list)
+        self.word_guessed = ["_"]* len(self.word)
+        self.num_letters  = len(set(self.word))
+        self.list_of_guesses = []  
+
+
+    def check_guess(self, guess): #class methods
+    
+        if guess.lower() in self.word:
+            print(f'Good guess! {guess} is in the word.')
+        
+            for i, character in enumerate(self.word):
+                if character == guess:
+                    self.word_guessed[i] = guess
+            self.num_letters -= 1
+
+        else: 
+            self.num_lives -= 1
+            print(f"Sorry, {guess} is not in the word.")
+            print(f"You have {self.num_lives} lives left.") 
+        
+        self.list_of_guesses.append(guess)
+        print(self.word_guessed)
+    
+    def ask_for_input(self):
+        while True:
+            guess = input('Please guess a letter. ')
+            if not guess.isalpha() and len(guess) != 1:
+                 print('Invalid letter. Please, enter a single alphabetical character.')
+
+            elif guess in self.list_of_guesses:
+                 print('You already tried that letter!')
+
+            else:
+                 self.check_guess(guess)
+                 break
+                 
+
 
 
 def play_game(word_list):
